221f88a11baefbe025340921ec3e5737f2c9fd52
xiexiaolong <mgfjxxiexiaolong@gmail.com>
2016-06-27 08:31:02 +0800

    Initial Commit

---
+++ CoreTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreTextDemo
+//
+//  Created by 谢小龙 on 16/6/27.
+//  Copyright © 2016年 XXL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
